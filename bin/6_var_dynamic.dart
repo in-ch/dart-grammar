00842151533ded0c224627600a6471e54507a748
in-ch <sxin2949@naver.com>
2022-03-20 23:31:43 +0900

    #6 var, dynamic 타입

---
+++ bin/6_var_dynamic.dart
@@ -0,0 +1,19 @@
+void main() {
+  var name = "인쵸리";
+
+  print(name);
+
+  name = '하잉';
+
+  print(name);
+
+  // print = 2;  error!!
+
+  dynamic name2 = "인쵸리";
+
+  print(name2);
+
+  name2 = 2;
+
+  print(name2);
+}
