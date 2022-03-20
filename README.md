de483be515bf82fb36a11733629d59e02f649a02
in-ch <sxin2949@naver.com>
2022-03-20 23:21:24 +0900

    #4 Strings

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 # 3 Numbers 
 
 1) 정수는 int, 실수는 double 
+
+# 4 String 
+
+1) 텍스트는 String으로 선언
+2) 특이한 점은 대문자로 시작한다. 
+3) '',"" 안에 $로 쓰면 변수로 인식한다. javascript의 ${} (리터널 템블렛)과 비슷한 기능을 수행한다. 
+4) $기능은 String과 int, double 상관없이 쓸 수 있다. 
---
+++ bin/4_Strings.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "인쵸리";
+
+  print(name);
+
+  String name2 = "그녀는";
+  String sentence = "예쁩니다.";
+
+  print(name2 + sentence);
+  print('$name2 $sentence');
+
+  int count = 5;
+  print('$name2 $count입니다.');
+}
